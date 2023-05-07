efabc5f1041b81bccc1b41f5d9f704532c79c503
mamyToavina <mamytoavina77@gmail.com>
2023-05-07 23:43:36 +0100

    ajout de sprint2

---
+++ WEB-INF/classes/Mapping.java
@@ -0,0 +1,20 @@
+package etu1797.framework;
+
+
+public class Mapping {
+    private String className;
+    private String method;
+
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
