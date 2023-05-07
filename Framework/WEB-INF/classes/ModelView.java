bff498240d4450410348ecf43c9f8aa26ae4077a
mamyToavina <mamytoavina77@gmail.com>
2023-05-07 23:52:43 +0100

    ajout de sprint5

---
+++ Framework/WEB-INF/classes/ModelView.java
@@ -0,0 +1,16 @@
+package etu1797.framework;
+
+import java.util.HashMap;
+
+public class ModelView {
+    private String view;
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+}
---
+++ TestFramework/emp-list.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>EMPLOYE</title>
+    </head>
+    <body>
+        <h1>Felicitation fa mety aloha iny!!!</h1>      
+    </body>
+</html>
